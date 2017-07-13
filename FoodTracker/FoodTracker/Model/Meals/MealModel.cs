3fb7633292b7938d65af8e465bb806f3be93e524
Sverda <sverda300@gmail.com>
2017-07-13 06:45:50 +0200

    Dynamic page for meal input, part 1

---
+++ FoodTracker/FoodTracker/Model/Meals/MealModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodTracker.Model.Meals
+{
+    public class MealModel
+    {
+        public MealModel(string name, int weight)
+        {
+            this.name = name;
+            this.weight = weight;
+        }
+        private string name;
+        private int weight;
+        public string Name { get { return name; } set { name = value; } }
+        public int Weight { get { return weight; } set { weight = value; } }
+    }
+}
