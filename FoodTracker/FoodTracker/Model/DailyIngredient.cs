c14c5d23422134a214d7368fc0b5f99f680a9d58
Sverda <sverda300@gmail.com>
2017-07-30 17:59:57 +0200

    DailyIngredient class has been fully implemented

--- FoodTracker/FoodTracker/Model/DailyIngredient.cs
+++ FoodTracker/FoodTracker/Model/DailyIngredient.cs
@@ -11,15 +11,23 @@ namespace FoodTracker.Model
     {
         public DailyIngredient()
         {
-
+            ingredients = null;
+            date = DateTime.Now;
+            type = MealType.Breakfast;
         }
-        public DailyIngredient(Ingredient ingredient, DateTime date)
+        public DailyIngredient(IEnumerable<Ingredient> ingredients, DateTime date, MealType type)
         {
-
+            this.ingredients = ingredients as List<Ingredient>;
+            this.date = date;
+            this.type = type;
         }
+        private List<Ingredient> ingredients;
         [PrimaryKey, AutoIncrement]
         public int DailyIngredientId { get; set; }
-        public int IngredientId { get; set; }
+        public int IngredientId(int index)
+        {
+            return ingredients[index].Id;
+        }
         private DateTime date;
         public DateTime Date
         {
@@ -35,6 +43,24 @@ namespace FoodTracker.Model
                 }
 
                 date = value;
+                OnPropertyChange();
+            }
+        }
+        private MealType type;
+        public MealType Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                if (type == value)
+                {
+                    return;
+                }
+
+                type = value;
                 OnPropertyChange();
             }
         }
