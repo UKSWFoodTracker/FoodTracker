1ca6710be502ff40aa143859b8631617aa23c76a
Sverda <sverda300@gmail.com>
2017-09-29 16:46:16 +0200

    Added TimerProperties file

---
+++ FoodTracker/FoodTracker/ViewModel/TimerService/TimerProperties.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodTracker.ViewModel.TimerService
+{
+    class TimerProperties
+    {
+        public TimerProperties()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
